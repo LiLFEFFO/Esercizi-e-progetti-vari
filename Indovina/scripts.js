f4b13145b1a5875896458e86a37491a5c81ac007
LiLFEFFO <federicofongaro370@gmail.com>
2025-10-24 10:59:55 +0200

    Update scripts.js

--- Indovina/scripts.js
+++ Indovina/scripts.js
@@ -1,7 +1,12 @@
+function calcolaMaxTentativi(){
+    let max = max;
+    let MaxTentativi = ((max/diff)/2);
+}
 let diff = prompt("Scegli la difficoltà tra: Facile (1), Media (2) o Difficile (3)");
 let max;
 let numeroDaIndovinare;
 
+//verifica della difficoltà -->
 if (diff == 1) {
     max = 1000;
 } else if (diff == 2) {
@@ -13,17 +18,21 @@ if (diff == 1) {
     max = 1000;
 }
 
+let tentativi_max = calcolaMaxTentativi();
+
+//Richiesta del numero da parte del programma -->
 numeroDaIndovinare = Math.floor(Math.random() * max) + 1;
 let num = parseInt(prompt("Inserisci un numero tra 1 e " + max), 10);
-let tentativi = 1;
+MaxTentativi = (MaxTentativi-1);
 
+//verifica del valore del numero inserito e calcolo dei tentativi -->
 while (num !== numeroDaIndovinare) {
     if (num < numeroDaIndovinare) {
         num = parseInt(prompt("Numero troppo basso. Riprova: "), 10);
     } else if (num > numeroDaIndovinare) {
         num = parseInt(prompt("Numero troppo alto. Riprova: "), 10);
     }
-    tentativi++;
+    MaxTentativi = (MaxTentativi-1);
 }
 
 alert("Complimenti! Hai indovinato il numero " + numeroDaIndovinare + " in " + tentativi + " tentativi.");
