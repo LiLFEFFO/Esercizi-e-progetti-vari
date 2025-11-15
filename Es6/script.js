814edc1c36230d8973a55558b41f17fb41fc0b60
FedericoFongaro <federicofongaro370@gmail.com>
2025-11-15 11:01:07 +0100

    Add files via upload

---
+++ Es6/script.js
@@ -0,0 +1,7 @@
+let soggetti = ["mio zio ", " il professor Maniglio ", " Alvise ", " Il Gel Igenizzante "];
+let azioni = [" salta su ", " mangia ", " evoca ", " igenizza "]
+let oggetti = [" la pizza ", " il professor Costa ", " un merluzzo che si chiama Antonno ", " Ayano "]
+
+function oggetto(){
+    let soggetto = Math.floor(Math.random)
+}
